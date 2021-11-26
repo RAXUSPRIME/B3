b3e193c490665fddf154587c3ae624391ad9ec44
RAXUSPRIME <94928611+RAXUSPRIME@users.noreply.github.com>
2021-11-26 14:10:51 +0100

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'PutoBot',
+        ownerName: 'MCA',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
